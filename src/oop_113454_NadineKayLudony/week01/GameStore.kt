a5014c3d7a89806a91bdff65a9fa55a9c11dd37f
Nadinelud <nadineludony@gmail.com>
2026-02-06 21:06:49 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_113454_NadineKayLudony/week01/GameStore.kt
+++ src/oop_113454_NadineKayLudony/week01/GameStore.kt
@@ -3,5 +3,10 @@ package oop_113454_NadineKayLudony.week01
 fun main(){
     val gameTitle: String = "Minecraft"
     val price: Int = 300000
-
 }
+
+fun calculateDiscount(price: Int): Double =
+    if (price > 500000)
+        price - (price * 0.2)
+    else
+        price - (price * 0.1)
