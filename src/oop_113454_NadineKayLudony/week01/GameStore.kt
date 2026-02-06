63294f03dd1e0a2ec506527974f76e40c00d3921
Nadinelud <nadineludony@gmail.com>
2026-02-06 21:14:04 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_113454_NadineKayLudony/week01/GameStore.kt
+++ src/oop_113454_NadineKayLudony/week01/GameStore.kt
@@ -1,12 +1,19 @@
 package oop_113454_NadineKayLudony.week01
 
 fun main(){
-    val gameTitle: String = "Minecraft"
-    val price: Int = 300000
+    val gameTitle: String
+    val price: Int
+    price = calculateDiscount(price = 500000)
+    printReceipt(gameTitle = "Minecraft", price)
 }
 
-fun calculateDiscount(price: Int): Double =
+fun calculateDiscount(price: Int): Int =
     if (price > 500000)
-        price - (price * 0.2)
+        price - (price * 20/100)
     else
-        price - (price * 0.1)
+        price - (price * 10/100)
+
+fun printReceipt(gameTitle: String, price:Int){
+    println(gameTitle)
+    println("Price: Rp. $price")
+}
