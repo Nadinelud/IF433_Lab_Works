3fda12300fdabc5dd9c847face0f8f4124b82b9b
Nadinelud <nadineludony@gmail.com>
2026-02-10 15:19:17 +0700

    week02: add warning validation in init block

--- src/oop_113454_NadineKayLudony/week02/student.kt
+++ src/oop_113454_NadineKayLudony/week02/student.kt
@@ -1,9 +1,12 @@
 package oop_113454_NadineKayLudony.week02
 
-class student(
-    val name: String,
-    val nim: String,
-    val major: String
-){
-
+class student(val name: String, val nim: String, val major: String){
+    init{
+        if(nim.length != 5){
+            println("WARNING: NIM $nim TIDAK VALID!")
+            println("Data mahasiswa $name akan menjadi tidak valid!")
+        } else {
+            println("LOG: Student $name berhasil ditambahkan")
+        }
+    }
 }
