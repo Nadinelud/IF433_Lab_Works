a8f1d2c9f1db7f83a8ab1f3a0631c63562d364ee
Nadinelud <nadineludony@gmail.com>
2026-02-20 15:28:21 +0700

    week03: (task 2) create Player class with private xp

---
+++ src/oop_113454_NadineKayLudony/week03/Player.kt
@@ -0,0 +1,22 @@
+package oop_113454_NadineKayLudony.week03
+
+class Player {
+    var username: String = "Dinalud"
+    private var xp: Int = 0
+    val level: Int
+        get() = (xp/100) + 1
+
+    fun addXp(amount: Int){
+        if(amount < 0){
+            println("Xp tidak valid!")
+        } else {
+            val currLevel = level
+            xp += amount
+            if(level > currLevel){
+                println("Level Up! Selamat $username naik ke level $level")
+            }
+        }
+
+
+    }
+}
