531cb4701ce806aa8e74e439497194ee3bdd47fe
Nadinelud <nadineludony@gmail.com>
2026-02-20 15:05:30 +0700

    week03: test all encapsulation logic in main

--- src/oop_113454_NadineKayLudony/week03/Main.kt
+++ src/oop_113454_NadineKayLudony/week03/Main.kt
@@ -1,7 +1,13 @@
 package oop_113454_NadineKayLudony.week03
 
 fun main(){
-    val e = Employee()
-    println("---Test Error ---")
-    e.salary = 5000
+    val e = Employee("Nadine")
+
+    e.salary = -1000
+    e.salary = 50000000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+
+    println("Pajak yang harus dibayar: ${e.tax}")
 }
