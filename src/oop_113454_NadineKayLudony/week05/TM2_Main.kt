947d55db6d487a5a1502cb529fad826cf5eb05fa
Nadinelud <nadineludony@gmail.com>
2026-03-03 22:57:38 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_113454_NadineKayLudony/week05/TM2_Main.kt
@@ -0,0 +1,12 @@
+package oop_113454_NadineKayLudony.week05
+
+fun main(){
+    val eWallet = EWallet("Nadine", 50000.0)
+    val cc = creditCard("nadine", 100000.0)
+
+    val paymentMethod: List<PaymentMethod> = listOf(eWallet, cc)
+
+    for(method in paymentMethod){
+        method.processPayment(75000.0)
+    }
+}
