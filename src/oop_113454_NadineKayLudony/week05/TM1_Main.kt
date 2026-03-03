286daa943b81578f7e16118036e0bb4ac63fbaef
Nadinelud <nadineludony@gmail.com>
2026-03-03 22:41:32 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_113454_NadineKayLudony/week05/TM1_Main.kt
@@ -0,0 +1,8 @@
+package oop_113454_NadineKayLudony.week05
+
+fun main(){
+    val math = MathHelper()
+    println("Luas persegi dengan sisi 5= ${math.hitungLuas(5)}")
+    println("Luas persegi panjang 4 x 5 = ${math.hitungLuas(4, 5)}")
+    println("Luas lingkaran dengan radius 7 = ${math.hitungLuas(7.0)}")
+}
