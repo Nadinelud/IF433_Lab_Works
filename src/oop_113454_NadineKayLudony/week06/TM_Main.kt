9438cdff65ff2f09e47219696bc65bd5c44185fa
Nadinelud <nadineludony@gmail.com>
2026-03-10 15:45:36 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_113454_NadineKayLudony/week06/TM_Main.kt
@@ -0,0 +1,7 @@
+package oop_113454_NadineKayLudony.week06
+
+fun main(){
+    val lamp = SmartLamp("1","Ruang Tamu")
+    val speaker = SmartSpeaker("1","Google Nest Dapur")
+    val CCTV = SmartCCTV("1", "Ezviz Garasi")
+}
