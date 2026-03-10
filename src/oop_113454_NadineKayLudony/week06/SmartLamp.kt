0e1cf2aebef6028c128a1cb31cbf187c4cbc5efe
Nadinelud <nadineludony@gmail.com>
2026-03-10 15:23:26 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_113454_NadineKayLudony/week06/SmartLamp.kt
@@ -0,0 +1,13 @@
+package oop_113454_NadineKayLudony.week06
+
+class SmartLamp(
+    override val id: String,
+    override val name: String
+): SmartDevice, Switchable {
+    override fun turnOn() {
+        println("Lampu $name menyala")
+    }
+    override fun turnOff() {
+        println("Lampu $name dimatikan")
+    }
+}
