fca98cd930e3d77c7fd6d4a0a1ba0b5b0bf767c0
Nadinelud <nadineludony@gmail.com>
2026-03-10 15:03:45 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_113454_NadineKayLudony/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_113454_NadineKayLudony.week06
+
+interface Camera{
+    fun turnOn(){
+        println("Lensa kamera terbuka dan sensor aktif")
+    }
+}
+
+interface Phone{
+    fun turnOn(){
+        println("Mencari sinyal...")
+    }
+}
