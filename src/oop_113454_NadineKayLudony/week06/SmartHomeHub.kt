1250092a4ea4436a6e240c3e1aeb344d774b884a
Nadinelud <nadineludony@gmail.com>
2026-03-10 15:47:37 +0700

    week06: (task) test smart home system logic in main

--- src/oop_113454_NadineKayLudony/week06/SmartHomeHub.kt
+++ src/oop_113454_NadineKayLudony/week06/SmartHomeHub.kt
@@ -3,7 +3,9 @@ package oop_113454_NadineKayLudony.week06
 class SmartHomeHub {
     val devices = mutableListOf<SmartDevice>()
 
-    fun addDevice(device: SmartDevice){}
+    fun addDevice(device: SmartDevice){
+        devices.add(device)
+    }
 
     fun turnOffAllSwitches(){
         for (device in devices){
--- src/oop_113454_NadineKayLudony/week06/TM_Main.kt
+++ src/oop_113454_NadineKayLudony/week06/TM_Main.kt
@@ -4,4 +4,13 @@ fun main(){
     val lamp = SmartLamp("1","Ruang Tamu")
     val speaker = SmartSpeaker("1","Google Nest Dapur")
     val CCTV = SmartCCTV("1", "Ezviz Garasi")
+
+    val hub = SmartHomeHub()
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(CCTV)
+
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
+
 }
