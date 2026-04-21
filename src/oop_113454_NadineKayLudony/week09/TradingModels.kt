b505c2727bf68de3c97134cc40ac13d5411ed3ea
Nadinelud <nadineludony@gmail.com>
2026-04-21 16:03:58 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_113454_NadineKayLudony/week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_113454_NadineKayLudony.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
