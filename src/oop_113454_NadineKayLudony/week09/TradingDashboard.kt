1a602794170c8004f22d87e7d3b0569192c7411e
Nadinelud <nadineludony@gmail.com>
2026-04-21 16:06:11 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_113454_NadineKayLudony/week09/TradingDashboard.kt
@@ -0,0 +1,15 @@
+package oop_113454_NadineKayLudony.week09
+
+package oop_<nim>_<nama>.week09
+
+fun main() {
+
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 20, 15.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -5.2, "CLOSED"),
+        TradeLog("BTCUSDT", "SHORT", 15, -10.0, "CLOSED"),
+        TradeLog("ETHUSDT", "LONG", 25, 20.0, "CLOSED"),
+        TradeLog("SOLUSDT", "LONG", 10, 8.0, "OPEN"),
+        TradeLog("BTCUSDT", "LONG", 5, -2.0, "OPEN")
+    )
+}
