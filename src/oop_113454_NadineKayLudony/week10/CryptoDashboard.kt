1916ed7ec20c23882cae0a44f1bb2ac523413194
Nadinelud <nadineludony@gmail.com>
2026-04-28 15:46:39 +0700

    week10: (task) populate coin repository

--- src/oop_113454_NadineKayLudony/week10/CryptoDashboard.kt
+++ src/oop_113454_NadineKayLudony/week10/CryptoDashboard.kt
@@ -2,5 +2,8 @@ package oop_113454_NadineKayLudony.week10
 
 fun main (){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 0.7))
+    coinRepo.add(Coin("ETH", 0.45))
+    coinRepo.add(Coin("USDT", 0.85))
 
 }
