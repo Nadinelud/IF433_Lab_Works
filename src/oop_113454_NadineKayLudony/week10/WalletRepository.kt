536844c0c93717ec4a2bd25cec9c0e619c8f2399
Nadinelud <nadineludony@gmail.com>
2026-04-28 15:43:37 +0700

    week10: (task) implement search logic in repository

--- src/oop_113454_NadineKayLudony/week10/WalletRepository.kt
+++ src/oop_113454_NadineKayLudony/week10/WalletRepository.kt
@@ -1,5 +1,7 @@
 package oop_113454_NadineKayLudony.week10
 
+import java.util.function.Predicate
+
 class WalletRepository<T>  {
     private val items = mutableListOf<T>()
     fun add(item: T){
@@ -7,6 +9,9 @@ class WalletRepository<T>  {
     }
     fun getAll(): List<T>{
         return items
+    }
+    fun search(predicate: (T) -> Boolean): List<T>{
+        return items.filter(predicate)
     }
 
 }
