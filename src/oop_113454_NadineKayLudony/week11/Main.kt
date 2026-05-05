cbe8295ee7865b3f485ced9e1cb74e395aa06f93
Nadinelud <nadineludony@gmail.com>
2026-05-05 14:53:21 +0700

    week11: test extension functions in main

---
+++ src/oop_113454_NadineKayLudony/week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_113454_NadineKayLudony.week11
+
+fun main(){
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alexs".addGreeting())
+    println("Hi ".repeatTimes(3))
+
+    val text: String? = null
+    println("Apakah null? ${text.isNullorEmptyCustom()}")
+}
