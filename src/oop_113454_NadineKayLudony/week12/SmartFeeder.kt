92b9a2f64d4b78d663a300777b057f492054d975
Nadinelud <nadineludony@gmail.com>
2026-05-12 22:19:52 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_113454_NadineKayLudony/week12/SmartFeeder.kt
@@ -0,0 +1,12 @@
+package oop_113454_NadineKayLudony.week12
+
+fun dispenseKibble(
+    requestedGram: Int,
+    availableGram: Int,
+    isJammed: Boolean
+): Int {
+    require(requestedGram > 0){
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    return availableGram-requestedGram
+}
