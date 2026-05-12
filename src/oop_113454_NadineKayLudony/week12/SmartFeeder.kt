d65c9cd9c63730e6a4b40f1859ae5e29c69bad3a
Nadinelud <nadineludony@gmail.com>
2026-05-12 22:24:24 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_113454_NadineKayLudony/week12/SmartFeeder.kt
+++ src/oop_113454_NadineKayLudony/week12/SmartFeeder.kt
@@ -10,5 +10,8 @@ fun dispenseKibble(
     }
     if(isJammed)
         throw DispenserJamException()
+    if(requestedGram > availableGram)
+        throw FoodEmptyException(requestedGram, availableGram)
+    println("Kibble berhasil dikeluarkan!")
     return availableGram-requestedGram
 }
