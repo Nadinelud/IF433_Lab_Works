ef75b8c39dd80ac7416cf78753af8d73b6a48ad8
Nadinelud <nadineludony@gmail.com>
2026-05-19 20:25:52 +0700

    week13: (task) calculate total PnL from loaded trades

--- src/oop_113454_NadineKayLudony/week13/CryptoTradeLogger.kt
+++ src/oop_113454_NadineKayLudony/week13/CryptoTradeLogger.kt
@@ -56,4 +56,7 @@ fun main (){
     saveTrades(trades, "crypto_trades.csv")
 
     File("crypto_trades.csv").appendText("CORRUPT_ID,DOGEUSDT,Hold,XX\n,YY\\n\"")
+
+    val loadedData = loadTrades("crypto_trades.csv")
+    val sumPnl = loadedData.sumOf { it.pnl }
 }
